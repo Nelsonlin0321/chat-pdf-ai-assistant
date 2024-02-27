992137c63e4b288f9d05847896abb6fde338740e
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-02-27 19:00:57 +0800

    update chat layout

---
+++ src/chat/[chatId]/page.tsx
@@ -0,0 +1,47 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+const ChatPage = async ({ params: { chatId } }: Props) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("./sign-in");
+  }
+
+  const _chats = await db.select().from(chats).where(eq(chats.userId, userId));
+
+  if (!_chats) {
+    return redirect("/");
+  }
+
+  if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="flex max-h-screen overflow-scroll">
+      <div className="flex w-full  max-h-screen overflow-scroll">
+        {/* chat side bar */}
+        <div className="flex-[1] max-w-xs">{/* chat side bar */}</div>
+        <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+          {/* PDF Viewer*/}
+        </div>
+        <div className="flex-[3] border-1-4 border-l-slate-200">
+          {/* ChatCOmponent*/}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
