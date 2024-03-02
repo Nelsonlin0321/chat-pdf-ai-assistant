e9e95219220022954605c9230b1f198e5e8914d3
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-02 11:10:46 +0800

    Add chat component

--- src/app/components/ChatComponent.tsx
+++ src/app/components/ChatComponent.tsx
@@ -1,9 +1,48 @@
+"use client";
+import { Input } from "./ui/input";
 import React from "react";
+import { useChat } from "ai/react";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
+import MessageList from "./MessageList";
 
 type Props = {};
 
 const ChatComponent = (props: Props) => {
-  return <div>ChatComponent</div>;
+  const { input, handleInputChange, handleSubmit, messages, isLoading } =
+    useChat({ api: "api/chat" });
+  return (
+    <div className="relative max-h-screen">
+      {/* header */}
+      <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
+        <h3 className="text-xl font-bold">Chat Component</h3>
+      </div>
+
+      <MessageList
+        messages={[
+          { id: "1", content: "Hi", role: "user" },
+          { id: "2", content: "How can I help you?", role: "assistant" },
+        ]}
+      />
+
+      <form
+        className="sticky bottom-0 inset-x-0 px-2 py-10 bg-white"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex gap-2 w-full">
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Ask any question ..."
+            className="mb-4"
+          />
+          <Button className="bg-blue-600" disabled={isLoading} type="submit">
+            <Send className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ChatComponent;
---
+++ src/app/components/MessageList.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { Message } from "ai";
+import React from "react";
+
+type Props = {
+  messages: Message[];
+};
+
+const MessageList = ({ messages }: Props) => {
+  if (!messages) return <></>;
+
+  return (
+    <div className="flex flex-col gap-2 px-4 py-2">
+      {messages.map((message) => {
+        return (
+          <div
+            key={message.id}
+            className={cn("flex", {
+              "justify-end pl-10": message.role === "user",
+              "justify-start pr-10": message.role === "assistant",
+            })}
+          >
+            <div
+              className={cn(
+                "rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10",
+                {
+                  " bg-blue-600 text-white": message.role === "user",
+                }
+              )}
+            >
+              <p>{message.content}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MessageList;
