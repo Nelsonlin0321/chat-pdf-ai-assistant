9f4daeca73b121e47ea6425ffb13782436dfb18c
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-13 21:28:15 +0800

    add health-check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook", "/api/health-check"],
 });
 
 export const config = {
