9e21444191c447c52de4941842030d315033321a
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-15 02:29:15 +0800

    update deletion

--- src/app/api/chat/[chatId]/route.ts
+++ src/app/api/chat/[chatId]/route.ts
@@ -14,23 +14,23 @@ export async function DELETE(request: NextRequest, { params }: Props) {
     select: { fileKey: true },
   });
 
-  await prisma.chat.deleteMany({
-    where: { chatId: chatId },
-  });
+  // await prisma.message.deleteMany({
+  //   where: { chatId: chatId },
+  // });
 
-  await prisma.message.deleteMany({
-    where: { chatId: chatId },
-  });
-
-  await prisma.embedding.deleteMany({
-    where: { chatId: chatId },
-  });
+  // await prisma.embedding.deleteMany({
+  //   where: { chatId: chatId },
+  // });
 
   if (chatFileKey) {
     const fileKey = chatFileKey.fileKey;
     await prisma.uploadedFile.deleteMany({ where: { fileKey: fileKey } });
     apiClient.delete("/delete_file", { data: { file_key: fileKey } });
   }
+
+  await prisma.chat.delete({
+    where: { chatId: chatId },
+  });
 
   return NextResponse.json(
     { message: "Deleted successfully" },
