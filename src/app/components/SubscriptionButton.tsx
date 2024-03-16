2fea6a539c91a801290b52247448ff875018e8ec
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-16 12:16:43 +0800

    Loading Effect

--- src/app/components/SubscriptionButton.tsx
+++ src/app/components/SubscriptionButton.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import axios from "axios";
+import { Loader2 } from "lucide-react";
 
 const SubscriptionButton = ({ isPro }: { isPro: boolean }) => {
   const [loading, setLoading] = useState(false);
@@ -17,13 +18,15 @@ const SubscriptionButton = ({ isPro }: { isPro: boolean }) => {
     }
   };
 
+  const label = isPro ? "Manage Subscription" : "Get Started";
+
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full bottom-0"
       onClick={async () => await handelSubscription()}
       disabled={loading}
     >
-      {isPro ? "Manage Subscription" : "Get Started"}
+      {loading ? <Loader2 className="animate-spin" /> : label}
     </button>
   );
 };
