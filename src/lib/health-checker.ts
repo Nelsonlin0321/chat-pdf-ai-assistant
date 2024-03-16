dba6ecb1b19037f516f0125c07e0815cc2856b35
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-17 00:07:41 +0800

    log api type of health check

--- src/lib/health-checker.ts
+++ src/lib/health-checker.ts
@@ -3,7 +3,7 @@ import { cloudRunApiClient, lambdaApiClient } from "./api-clients";
 export const getCloudRunHealthStatus = async () => {
   try {
     const response = await cloudRunApiClient.get("/health_check");
-    console.log(response.data);
+    console.log({ API: "Cloud Run", ...response.data });
   } catch (error) {
     console.error("Cloud Run Error fetching health status:", error);
   }
@@ -12,7 +12,7 @@ export const getCloudRunHealthStatus = async () => {
 export const getLambdaHealthStatus = async () => {
   try {
     const response = await lambdaApiClient.get("/health_check");
-    console.log(response.data);
+    console.log({ API: "Lambda", ...response.data });
   } catch (error) {
     console.error("Lambda Error fetching health status:", error);
   }
